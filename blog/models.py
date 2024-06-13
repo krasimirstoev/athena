cedd56b21f1c3307e76ce1419010d0f02c77061b
Krasimir Stoev <alabalism@gmail.com>
2024-06-13 15:06:17 +0300

    Adding save method to Page class in order to make valid tests in the future.

--- blog/models.py
+++ blog/models.py
@@ -67,6 +67,11 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
         return f'/{self.slug}/'
 
